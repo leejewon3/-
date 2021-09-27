beb4e76050b13625d659bccf2a0233f1ac14a4a5
leejewon3 <leejewon3@kookmin.ac.kr>
2021-09-27 23:22:47 +0900

    05P12

---
+++ 05_practice_2.ino
@@ -0,0 +1,31 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
+}
+
+void loop() {
+  for ( int i = 0 ; i < 10 ; i++)
+  {
+    toggle = toggle_state(toggle); //toggle LED value.
+    digitalWrite(PIN_LED, toggle); // update LED status.
+    delay(400); // wait for 100 milliseconds
+  }
+  while(1)
+  {
+    digitalWrite(PIN_LED, 1);
+  }
+}
+
+int toggle_state(int toggle) {
+  if (toggle==0){
+    toggle=1;
+  }
+  else {
+    toggle=0;
+  }
+  return toggle;
+}
